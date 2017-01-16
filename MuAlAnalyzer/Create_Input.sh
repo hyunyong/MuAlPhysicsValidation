2ce96cda036e702d2f54335a19c43e8be4b05f6d
Luca <luca.pernie@cern.ch>
2017-01-16 17:51:42 +0100

    fix legend

--- MuAlAnalyzer/Create_Input.sh
+++ MuAlAnalyzer/Create_Input.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 eosComm="/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select"
-Folder="/store/group/alca_muonalign/lpernie/MuAlRefit_Run2016G_RAWreco_DT6DOF_CSC3DOF_diaAPE_01"
-fileTXT="MuAlRefit_Run2016G_RAWreco_DT6DOF_CSC3DOF/MuAlRefit_Run2016G_RAWreco_DT6DOF_CSC3DOF_list.py"
+Folder="/store/group/alca_muonalign/lpernie/MuAlRefit_Run2016G_RAWreco_DT3DOF_CSC3DOF_Z15mmsmean_oldGPR_01/SingleMuon/crab_MuAlRefit_Run2016G_RAWreco_DT3DOF_CSC3DOF_Z15mmsmean_oldGPR_01/170112_014955"
+fileTXT="MuAlRefit_Run2016G_RAWreco_DT3DOF_CSC3DOF_Z155mmOldGPR/MuAlRefit_Run2016G_RAWreco_DT3DOF_CSC3DOF_Z155mmOldGPR_list.py"
+#Folder="/store/group/alca_muonalign/lpernie/MuAlRefit_Run2016G_RAWreco_DT6DOF_CSC3DOF_APE_Z15mmsmean_oldGPR_01/SingleMuon/crab_MuAlRefit_Run2016G_RAWreco_DT6DOF_CSC3DOF_APE_Z15mmsmean_oldGPR_01/170112_155326"
+#fileTXT="MuAlRefit_Run2016G_RAWreco_DT6DOF_CSC3DOF_Z155mmOldGPR/MuAlRefit_Run2016G_RAWreco_DT6DOF_CSC3DOF_Z155mmOldGPR_list.py"
+
 
 listfile=`$eosComm find $Folder | grep root | grep -v failed`
 echo 'fileNames = [' > $fileTXT

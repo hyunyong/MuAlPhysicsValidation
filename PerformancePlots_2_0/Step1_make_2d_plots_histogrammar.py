87fa3992d6ee50c9faf8643051b22b490448ceb7
Adrian Thompson <31362409+athompson-tamu@users.noreply.github.com>
2018-01-11 13:20:52 -0600

    Update Step1_make_2d_plots_histogrammar.py

--- PerformancePlots_2_0/Step1_make_2d_plots_histogrammar.py
+++ PerformancePlots_2_0/Step1_make_2d_plots_histogrammar.py
@@ -17,7 +17,7 @@ import time
 fileIn = sys.argv[1]
 fileOut = sys.argv[2]
 method = sys.argv[3]
-#TODO(Adrian/Ryan): fix oldTTrees bug. Setting to True as a band-aid.
+# TODO(Adrian/Ryan): fix oldTTrees bug. Setting to True as a band-aid.
 oldTTrees = True #sys.argv[4]=="oldTTrees"
 if(method!="std" and method!="pos" and method!="neg" and method!="equal"):
   print "WARNIGN! method is unknown, options are std,pos,neg,equal";

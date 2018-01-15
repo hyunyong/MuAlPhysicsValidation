145d1e5aa7905f88c04c6feff61fc191c881088f
Adrian Thompson <31362409+athompson-tamu@users.noreply.github.com>
2018-01-15 15:58:50 -0600

    removing oldTTrees

--- PerformancePlots_2_0/Step1_make_2d_plots.py
+++ PerformancePlots_2_0/Step1_make_2d_plots.py
@@ -3,22 +3,23 @@ from random import randint
 import math, sys, os
 from math import fabs
 execfile("constants.py")
-#RUN WITH:
-#python Step1_make_2d_plots.py inputFile outputFolder Treating_Mu_AntiMU (std, use only positive, only negative, equal number)
-##python Step1_make_2d_plots.py /afs/cern.ch/work/l/lpernie/MuonAlign/WD/CMSSW_9_4_0_pre3/src/MuAlPhysicsValidation/MuAlAnalyzer/MuAlRefit_Run2017part1_RAWreco_rerecov2 MuAlRefit_Run2017part1_RAWreco_rerecov2 std -b # std,pos,neg,equal
-#python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWreco_2016Geom_01.root MuAlRefit_2017B_2016Geom std -b # std,pos,neg,equal
-#python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Legacy.root MuAlRefit_2016Perform std -b # std,pos,neg,equal
-#python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWreco_DT6DOFCSC3DOF.root MuAlRefit_2017B_DT6DOFCSC3DOF std -b
-#python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWRECO_DT3DOFnoT0it1CSC3DOF.root MuAlRefit_2017B_DT3DOFnoT0it1CSC3DOF std -b
-#python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWRECO_DT3DOFnoT0it1CSC3DOF_1refit.root MuAlRefit_2017B_DT3DOFnoT0it1CSC3DOF_1refit std -b
-#python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWRECO_DT6DOFnoT0it1CSC3DOF_Official.root MuAlRefit_2017B_DT6DOFnoT0it1CSC3DOF_Official std -b
-#python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWRECO_DT6DOFnoT0it1CSC3DOF_OfficialAPEDTminuit_CSCasym.root MuAlRefit_2017B_DT6DOFnoT0it1CSC3DOF_OfficialAPEDTminuit_CSCasym std -b
-#python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWreco_FromGT.root MuAlRefit_2017B_fromGT std -b
+
+# Usage examples:
+# python Step1_make_2d_plots.py inputFile outputFolder Treating_Mu_AntiMU (std, use only positive, only negative, equal number)
+# python Step1_make_2d_plots.py /afs/cern.ch/work/l/lpernie/MuonAlign/WD/CMSSW_9_4_0_pre3/src/MuAlPhysicsValidation/MuAlAnalyzer/MuAlRefit_Run2017part1_RAWreco_rerecov2 MuAlRefit_Run2017part1_RAWreco_rerecov2 std -b # std,pos,neg,equal
+# python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWreco_2016Geom_01.root MuAlRefit_2017B_2016Geom std -b # std,pos,neg,equal
+# python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Legacy.root MuAlRefit_2016Perform std -b # std,pos,neg,equal
+# python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWreco_DT6DOFCSC3DOF.root MuAlRefit_2017B_DT6DOFCSC3DOF std -b
+# python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWRECO_DT3DOFnoT0it1CSC3DOF.root MuAlRefit_2017B_DT3DOFnoT0it1CSC3DOF std -b
+# python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWRECO_DT3DOFnoT0it1CSC3DOF_1refit.root MuAlRefit_2017B_DT3DOFnoT0it1CSC3DOF_1refit std -b
+# python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWRECO_DT6DOFnoT0it1CSC3DOF_Official.root MuAlRefit_2017B_DT6DOFnoT0it1CSC3DOF_Official std -b
+# python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWRECO_DT6DOFnoT0it1CSC3DOF_OfficialAPEDTminuit_CSCasym.root MuAlRefit_2017B_DT6DOFnoT0it1CSC3DOF_OfficialAPEDTminuit_CSCasym std -b
+# python Step1_make_2d_plots.py /eos/cms/store/group/alca_muonalign/lpernie/MuAlAnalyzer/MuAlRefit_Run2017B_RAWreco_FromGT.root MuAlRefit_2017B_fromGT std -b
+
 # input arguments, first is input root file, second is output root file 
 fileIn = sys.argv[1]
 fileOut = sys.argv[2]
 method = sys.argv[3]
-oldTTrees = sys.argv[4]=="oldTTrees"
 if(method!="std" and method!="pos" and method!="neg" and method!="equal"):
   print "WARNIGN! method is unknown, options are std,pos,neg,equal";
 

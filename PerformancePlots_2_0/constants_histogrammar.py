ceb624a6cb4f2b557d73513cc6894fab6b93c8d5
rymuelle <rmueller@physics.tamu.edu>
2018-07-12 21:48:40 +0200

    all plots in general folder are working

---
+++ PerformancePlots_2_0/constants_histogrammar.py
@@ -0,0 +1,133 @@
+verbosity = 0
+
+isMC = False
+
+ptBins = 48
+ptMin = 0
+ptMax = 200
+
+thresholdPt = 30.0
+
+phiBins = 48
+phiMin = -3.2
+phiMax = 3.2
+
+phiResBins = 48
+phiResMin = -100
+phiResMax = 100
+
+etaBins = 48
+etaMin = -2.4
+etaMax = 2.4
+
+ptResBins = 48
+ptResMin = -0.1
+ptResMax = 0.1
+#ptResMin = -0.7 for other definition as seen in code but commented out
+#ptResMax = 0.7
+
+
+massBins = 48
+massMin = 60
+massMax = 120
+
+hydridMassCut = 5.
+hybridMassValue = 91.
+
+
+nChi2Bins = 48
+nChi2Min = 0
+nChi2Max = 3
+
+nBins = 48
+
+nHitsRange = [0,75]
+
+nChi2Range = [0,4]
+
+ptPullRange = [-2.8, 2.8]
+ptResSTA = [-.75,.75]
+ptResSTAType2 = [-1,1]
+
+ptResGLB = [-.1,.1]
+
+deltaPhiRange = .03
+phiErrorRange = .001
+
+
+##############
+
+isMC = False
+#Step 1
+ptBins = 48
+ptMin = 0
+ptMax = 200
+thresholdPt = 30.0
+
+phiBins = 48
+phiMin = -3.2
+phiMax = 3.2
+phiResBins = 48
+phiResMin = -100
+phiResMax = 100
+
+etaBins = 48
+etaMin = -2.4
+etaMax = 2.4
+ptResBins = 48
+ptResMin = -0.1
+ptResMax = 0.1
+
+massBins = 48
+massMin = 60
+massMax = 120
+
+hydridMassCut = 10.
+hybridMassValue = 91.
+nChi2Bins = 48
+nChi2Min = 0
+nChi2Max = 3
+
+nBins = 48
+nHitsRange = [0,75]
+nChi2Range = [0,4]
+ptPullRange = [-2.8, 2.8]
+ptResSTA = [-.75,.75]
+ptResSTAType2 = [-1,1]
+ptResGLB = [-.1,.1]
+
+# Step 2
+nBins = 10
+nGausFit = 1.1
+drawBinPlots = True
+nBins = 16
+
+ptResMeanRange = -.003,.003
+ptResSigmaRange = .00, .008
+
+ptResGLBMeanRange = -.01,.01
+ptResGLBSigmaRange = .00, .2
+
+ptResSTAMeanRange = -.5,.5
+ptResSTASigmaRange = 0, .6
+
+ptResType2STAMeanRange = -.1,.1
+ptResType2STASigmaRange = 0, .3
+
+ptPullSTAMeanRange = -10,10
+ptPullSTASigmaRange = 0, 30
+
+ptPullGLBMeanRange = -.3,.3
+ptPullGLBSigmaRange = 0, 3
+
+massMeanRange = 80, 100
+massSigmaRange = 0, 20
+
+nChi2MeanRange = .5, 2
+nChi2RMSRange = 0, 1
+
+nHitsMeanRange = 30, 60
+nHitsRMSRange = 8, 12
+
+
+##############
--- PerformancePlots_2_0/histograms_histogrammar.py
+++ PerformancePlots_2_0/histograms_histogrammar.py
@@ -2,7 +2,7 @@ from ROOT import TH1F, TCanvas, TProfile, TProfile2D, TProfile3D, TH2F
 from array import array
 import numpy
 from histogrammar import *
-
+from constants_histogrammar import *
 
 
 Bundle = UntypedLabel
@@ -10,61 +10,6 @@ Bundle = UntypedLabel
 
 #leadingMuonPt = Bin("leadingMuonPt", "leading #mu p_{T}; p_{T}; counts", 100, 0, 1000)
 
-verbosity = 0
-
-isMC = False
-
-ptBins = 48
-ptMin = 0
-ptMax = 200
-
-thresholdPt = 30.0
-
-phiBins = 48
-phiMin = -3.2
-phiMax = 3.2
-
-phiResBins = 48
-phiResMin = -100
-phiResMax = 100
-
-etaBins = 48
-etaMin = -2.4
-etaMax = 2.4
-
-ptResBins = 48
-ptResMin = -0.1
-ptResMax = 0.1
-#ptResMin = -0.7 for other definition as seen in code but commented out
-#ptResMax = 0.7
-
-
-massBins = 48
-massMin = 60
-massMax = 120
-
-hydridMassCut = 5.
-hybridMassValue = 91.
-
-
-nChi2Bins = 48
-nChi2Min = 0
-nChi2Max = 3
-
-nBins = 48
-
-nHitsRange = [0,75]
-
-nChi2Range = [0,4]
-
-ptPullRange = [-2.8, 2.8]
-ptResSTA = [-.75,.75]
-ptResSTAType2 = [-1,1]
-
-ptResGLB = [-.1,.1]
-
-deltaPhiRange = .03
-phiErrorRange = .001
 
 standard_histograms = Select( lambda array: numpy.logical_and(array['glb_trk_pt'] > thresholdPt ,abs(array['glb_trk_eta']) < 2.4),
                         Select( lambda array: numpy.logical_and(array['glb'] == True ,abs(array['sta']) == True ),
@@ -82,8 +27,6 @@ D1_sta_trk_delta_phi_barrel = Select(lambda array: abs(array['glb_eta']) < .9 ,
 D1_sta_trk_delta_phi_endcap = Select(lambda array: abs(array['glb_eta']) > .9 , 
     Bin(phiResBins, phiResMin, phiResMax, lambda array : (array['glb_trk_phi']- array['sta_phi'])/array['glb_phi_error'], Count() )),
 
-
-
 D1_sta_glb_delta_phi = Bin( phiResBins, phiResMin, phiResMax, 
     lambda array : (array['glb_phi']- array['sta_phi'])/array['glb_phi_error'], Count() ),
 
@@ -96,7 +39,6 @@ D1_sta_glb_delta_phi_endcap = Select(lambda array: abs(array['glb_eta']) > .9 ,
 ##
 ## nchi2
 ##
-
 
 D1_glb_nChi2_barrel = Select(lambda array: abs(array['glb_eta']) < .9 , 
     Bin(nChi2Bins, nChi2Min, nChi2Max, lambda array : array['glb_nchi2'], Count() )),
@@ -113,7 +55,6 @@ D1_sta_nChi2_barrel = Select(lambda array: abs(array['glb_eta']) < .9 ,
 D1_sta_nChi2_endcap = Select(lambda array: abs(array['glb_eta']) > .9 , 
     Bin(nChi2Bins, nChi2Min, nChi2Max, lambda array : array['sta_nchi2'], Count() )),
 
-
 D2_glb_eta_nChi2 = Bin( etaBins, etaMin, etaMax, lambda array: array['glb_eta'], 
     Bin( nBins, nChi2Range[0], nChi2Range[1], lambda array: array['glb_nchi2'], Count() )),
 
@@ -123,17 +64,15 @@ D2_glb_pt_nChi2 = Bin( ptBins, ptMin, ptMax, lambda array: array['glb_pt'],
 D2_sta_pt_nChi2 = Bin( ptBins, ptMin, ptMax, lambda array: array['sta_pt'], 
     Bin( nBins, nChi2Range[0], nChi2Range[1], lambda array: array['glb_nchi2'], Count() )),
 
-D2_sta_eta_nChi2 = Bin( ptBins, ptMin, ptMax, lambda array: array['sta_eta'], 
-    Bin( nBins, nChi2Range[0], nChi2Range[1], lambda array: array['glb_nchi2'], Count() )),
+D2_sta_eta_nChi2 = Bin( etaBins, etaMin, etaMax, lambda array: array['sta_eta'], 
+    Bin( nBins, nChi2Range[0], nChi2Range[1], lambda array: array['sta_nchi2'], Count() )),
 
 ##
 ## nHits
 ##
 
-
-
-D2_glb_eta_nHits = Bin( 48, -2.4, 2.4, lambda array: array['glb_eta'], 
-    Bin( 24, 0, 75, lambda array: array['glb_nhits'], Count() )),
+D2_glb_eta_nHits = Bin( etaBins, etaMin, etaMax, lambda array: array['glb_eta'], 
+    Bin( 24, nHitsRange[0], nHitsRange[1], lambda array: array['glb_nhits'], Count() )),
 
 D2_glb_pt_nHits = Bin( ptBins, ptMin, ptMax, lambda array: array['glb_pt'], 
     Bin( nBins, nHitsRange[0], nHitsRange[1], lambda array: array['glb_nhits'], Count() )),
@@ -160,7 +99,6 @@ D2_glb_sta_phi_ptRes_endcap = Select(lambda array: abs(array['glb_eta']) > .9 ,
     Bin( phiBins, phiMin, phiMax, lambda array: array['glb_phi'], 
     Bin( ptResBins, ptResMin, ptResMax, lambda array: array['ptResSTAGLB'], Count() ))),
 
-
 ##
 ##  ptPull
 ##
@@ -173,8 +111,6 @@ D2_glb_sta_pt_ptPull = Bin( ptBins, ptMin, ptMax, lambda array: array['glb_pt'],
 
 D2_glb_sta_phi_ptPull = Bin( phiBins, phiMin, phiMax, lambda array: array['glb_phi'], 
     Bin( nBins, ptPullRange[0], ptPullRange[1], lambda array: array['ptPullSTAGLB'], Count() )),
-
-
 
 ))))
 
@@ -197,11 +133,9 @@ D2_gen_glb_phi_ptRes_barrel = Select(lambda array: abs(array['glb_eta']) < .9 ,
     Bin( phiBins, phiMin, phiMax, lambda array: array['glb_gen_phi'], 
     Bin( ptResBins, ptResMin, ptResMax, lambda array: array['ptResGLBGEN'], Count() ))),
 
-
 D2_gen_glb_phi_ptRes_endcap = Select(lambda array: abs(array['glb_eta']) > .9 , 
     Bin( phiBins, phiMin, phiMax, lambda array: array['glb_gen_phi'], 
     Bin( ptResBins, ptResMin, ptResMax, lambda array: array['ptResGLBGEN'], Count() ))),
-
 
 ##
 ##  ptPull
@@ -216,10 +150,7 @@ D2_gen_glb_pt_ptPull = Bin( ptBins, ptMin, ptMax, lambda array: array['glb_gen_p
 D2_gen_glb_phi_ptPull = Bin( phiBins, phiMin, phiMax, lambda array: array['glb_gen_phi'], 
     Bin( nBins, ptPullRange[0], ptPullRange[1], lambda array: array['ptPullGLBGEN'], Count() )),
 
-
-
 ))
-
 
 mass_histograms = Select( lambda array: numpy.logical_and(array['pos_glb_trk_pt'] > thresholdPt ,abs(array['pos_glb_trk_eta']) < 2.4), 
                    Select( lambda array: numpy.logical_and(array['neg_glb_trk_pt'] > thresholdPt ,abs(array['neg_glb_trk_eta']) < 2.4), 
